26ee8fa556d69cd3a5f0ac7b45ed74b3f133dc43
htn5 <htn@ucsb.edu>
2021-03-01 20:42:17 -0800

    left sidebar

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import Navbar from './Navbar';
 import Chat from './Chat';
+import LeftSidebar from './LeftSidebar';
 
 function HomePage({handleLogout}) {
     return(
-        <div class="container-fluid">
-            <Navbar handleLogout={handleLogout}/>
-            <div class="row justify-content-center" style={{height:"100vh"}} >
-                <div class="col-2"></div>
-                <div class="col-7"><Chat/></div>
-                <div class="col-3"></div>
+        <div>
+            <div class="container-fluid">
+                <Navbar handleLogout={handleLogout}/>
+                <div class="row justify-content-center" style={{height:"100vh"}} >
+                    <div class="col-2"><LeftSidebar /></div>
+                    <div class="col-7"><Chat/></div>
+                    <div class="col-3"></div>
+                </div>
             </div>
         </div>
     )
---
+++ src/components/LeftSidebar.js
@@ -0,0 +1,22 @@
+import React, {useState, useEffect} from 'react'
+import {auth} from "./Firebase";
+import {db} from "./Firebase";
+
+function test(){
+    document.getElementById("test").style.color = "red";
+}
+
+function LeftSidebar(){
+    return (
+        <div className="Left_Sidebar">
+            {/* <div className="container"> */}
+                {/* <div className="row-3"> */}
+                    <h3>Class Name</h3>
+                    <p id="test" onClick="test()">testnjewnfjasldkjfnwefns;aidfnienflasjfahewfn</p>
+                {/* </div> */}
+            {/* </div> */}
+        </div>
+    )
+}
+
+export default LeftSidebar
